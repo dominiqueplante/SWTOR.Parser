b239979fbedbd4006b6869532775961639fae304
Dominique Plante <dominique.plante@gmail.com>
2012-04-04 16:10:08 -0700

    move code out of Controller and into domain object where it seems more appropriate

--- src/SWTOR.Parser/Domain/CombatLog.cs
+++ src/SWTOR.Parser/Domain/CombatLog.cs
@@ -1,9 +1,13 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SWTOR.Parser.Domain
 {
+    // This POCO can easily be stored in RavenDB
     public class CombatLog : ILogMetrics
     {
         public CombatLog()
@@ -21,5 +25,41 @@ namespace SWTOR.Parser.Domain
         public int CountOfParry { get; set; }
         public int CountOfDeflect { get; set; }
         public List<AbilityMetrics> AbilityCounts { get; set; }
+
+        public static CombatLog CreateCombatLog(string combatLog)
+        {
+            string hash = ComputeHash(combatLog);
+            var logParser = new Parser();
+            var log = logParser.Parse(new StringReader(combatLog));
+            return CreateCombatLog(hash, log);
+        }
+
+        public static CombatLog CreateCombatLog(string hash, List<LogEntry> log)
+        {
+            var combatParser = new CombatParser();
+            var model = combatParser.Parse(log);
+
+            combatParser.Clean(model);
+            model.Id = hash;
+            return model;
+        }
+
+        public static string ComputeHash(string data)
+        {
+            var MD5 = new MD5CryptoServiceProvider();
+            char[] bArr = data.ToCharArray();
+            int size = bArr.GetUpperBound(0);
+            byte[] arr = new byte[size];
+            Encoding.Default.GetEncoder().GetBytes(bArr, 0, size, arr, 0, true);
+            var retVal = MD5.ComputeHash(arr);
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
     }
 }
--- src/SWTOR.Web/Controllers/CombatLogController.cs
+++ src/SWTOR.Web/Controllers/CombatLogController.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
-using System.IO;
 using Raven.Client;
 using SWTOR.Parser.Domain;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace SWTOR.Web.Controllers
 {
     public class CombatController : Controller
     {
-        private IDocumentSession session;
+        private readonly IDocumentSession session;
 
         public CombatController(IDocumentSession session)
         {
@@ -23,39 +17,14 @@ namespace SWTOR.Web.Controllers
         [HttpPost]
         public ActionResult Parse(string combatLog)
         {
-            string hash = ComputeHash(combatLog);
-            var logParser = new SWTOR.Parser.Parser();
-            var combatParser = new SWTOR.Parser.CombatParser();
-
-            var log = logParser.Parse(new StringReader(combatLog));
-            var model = combatParser.Parse(log);
-
-            combatParser.Clean(model);
-            model.Id = hash;
-
+            var model = CombatLog.CreateCombatLog(combatLog);
+            
             session.Store(model);
             session.SaveChanges();
 
             return RedirectToAction("Log", new { id = model.Id });
         }
 
-        private string ComputeHash(string data)
-        {
-            var MD5 = new MD5CryptoServiceProvider();
-            char[] bArr = data.ToCharArray();
-            int size = bArr.GetUpperBound(0);
-            byte[] arr = new byte[size];
-            System.Text.Encoding.Default.GetEncoder().GetBytes(bArr, 0, size, arr, 0, true);
-            var retVal = MD5.ComputeHash(arr);
-
-            var sb = new StringBuilder();
-            for (int i = 0; i < retVal.Length; i++)
-            {
-                sb.Append(retVal[i].ToString("x2"));
-            }
-
-            return sb.ToString();
-        }
         [HttpGet]
         public ActionResult Log(string id)
         {
